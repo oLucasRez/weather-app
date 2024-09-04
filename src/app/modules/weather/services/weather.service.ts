9d15738a2ce403afebd9aad9e5079d6bffc93bb4
Lucas Rezende <lucas.s.oliveira.rezende@gmail.com>
2024-09-04 14:55:13 -0300

    type weather-service

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,22 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { WeatherData } from 'src/app/models/interfaces/weather-data.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  private apiKey = 'a611c09621c7443f0ff2e9d8b351707e';
+  private apiKey = 'aaaabbbbccccddddeeeeffffgggghhhh';
 
   constructor(private http: HttpClient) {}
 
-  getWeatherData(cityName: string): Observable<any> {
+  getWeatherData(cityName: string): Observable<WeatherData> {
     const url = new URL('https://api.openweathermap.org/data/2.5/weather');
     url.searchParams.append('q', cityName);
     url.searchParams.append('units', 'metric');
     url.searchParams.append('mode', 'json');
     url.searchParams.append('appid', this.apiKey);
 
-    return this.http.get(url.toString(), {});
+    return this.http.get<WeatherData>(url.toString(), {});
   }
 }
